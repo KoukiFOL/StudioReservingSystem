6bc1f8b430c250caae074f08a7afb2831d8b2d15
Kouki_FOL <kohki_ueyama@yahoo.co.jp>
2023-05-28 21:53:28 +0900

    コントローラを少しいじった 「スタジオ予約システム」から「軽音ポータル」に変更（ほかにも機能の追加を考慮）

--- StudioReservingSystem/config/routes.rb
+++ StudioReservingSystem/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
+  get "/ueer/new" => "user#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/about' => "home#about"
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/login' => 'user#login'
+  get "/new" => 'user#new'
+  get '/' => "home#top"
+  
 end
